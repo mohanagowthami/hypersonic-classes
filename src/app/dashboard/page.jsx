f61863f46b4825d65f7c4601cd3adc6b63ca9149
gowthami <mohanagajula579@gmail.com>
2025-01-26 12:19:14 +0530

    map function

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,10 +1,18 @@
 import X, { Y } from "../components/NewHeader"
 
 const Dashboard=()=>{
+
+
+    const list=[1,2,3,4,5,"new array element"]
     return <div> 
     
     <p> this is dashboard</p>
     <p>this is another </p>
+
+    { list.map((ele, index)=>{
+        return <p >{ele+2}</p>
+
+    })}
     <X/>
    <Y/>
     </div>
