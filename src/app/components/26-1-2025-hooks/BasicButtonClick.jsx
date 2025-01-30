74bb09d634a875a61e6526154307b717b377f2ed
gowthami <mohanagajula579@gmail.com>
2025-01-30 11:40:08 +0530

    asynchronous operations in react

--- src/app/components/26-1-2025-hooks/BasicButtonClick.jsx
+++ src/app/components/26-1-2025-hooks/BasicButtonClick.jsx
@@ -1,28 +1,54 @@
 "use client"
 
 import { useEffect, useState } from "react"
-
-
 // if want to update Ui , dom should be rerender( re execute), in react, if you want to rerender the Ui , the only way is you need to update state variable value ,==> when every state variable values changes, dom will rerender and UPdates the UI
-
 export const BasicButtonClick = () => {
-    const [text, setText] = useState("This is my para graph")
-    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    const [ stateList, setList]=useState(list)
-    
-    const handleClick = () => {
+  const [text, setText] = useState("This is my para graph")
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [stateList, setList] = useState(list)
+  const [todos, setTodos]=useState(null)
+
+  const handleClick = () => {
     setText("updated new paragraph text")
-    }// to perform side effects like data fetching and to update the Ui again
-    //
-   useEffect(() => {
-    }, [])
-    const handleElementClick = (e, index) => {
-      list[index] = list[index] + 1
-      setList([...list])
+  }// to perform side effects like data fetching and to update the Ui again
+  //
+
+
+const fetchingDataFromAPI = async () => {
+    try {
+      let response = await fetch("https://jsonplaceholder.typicode.com/todos")
+      response = await response.json()
+      console.log(response, "response from api")
+      if(response)
+      {
+        setTodos(response)
+      }
     }
-    return <div>
-        <p>{text}</p>
-        <button onClick={handleClick}> This is my button</button>
-        {list.map((ele, index) => <p onClick={(e) => handleElementClick(e, index)}>{ele}</p>)}
- </div>
-}
+    catch (error) {
+    }
+  }
+  // mounting phase
+  useEffect(() => {
+    fetchingDataFromAPI()
+  }, [])
+
+
+   // updating phase
+   /useEffect(() => {
+    console.log(text, "text")
+  }, [text, stateList])
+
+
+  const handleElementClick = (e, index) => {
+    list[index] = list[index] + 1
+    setList([...list])
+  }
+  return <div>
+    <p>{text}</p>
+    <button onClick={handleClick}> This is my button</button>
+    {list.map((ele, index) => <p onClick={(e) => handleElementClick(e, index)}>{ele}</p>)}
+    {todos[0].map((ele, index)=>{ 
+        return <p key={index}> {ele.title}</p>
+})}
+  </div>
+} 
