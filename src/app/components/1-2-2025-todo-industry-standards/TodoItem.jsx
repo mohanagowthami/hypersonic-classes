7c78b3c0f1071c4abf4558be8af47ee4baab92a4
gowthami <mohanagajula579@gmail.com>
2025-02-06 11:38:13 +0530

    vice versa of todo completion

--- src/app/components/1-2-2025-todo-industry-standards/TodoItem.jsx
+++ src/app/components/1-2-2025-todo-industry-standards/TodoItem.jsx
@@ -2,18 +2,21 @@ export const TodoItem = (props) => {
 
     console.log(props, "props in todoItem")
 
-    const { todoData, handleDeleteFromParent,handleCompleteTodotaskProp } = props
+    const { todoData, handleDeleteFromParent, handleCompleteTodotaskProp } = props
 
     const handleDeleteButton = () => {
         handleDeleteFromParent()
     }
 
 
+
+
+
     return <div className="flex gap-6 items-center justify-center">
 
         <p>{todoData.name}</p>
-        {todoData.isCompleted ? null:
-        <button   onClick={handleCompleteTodotaskProp}>complete todo</button>}
+        {todoData.isCompleted ?  <button onClick={handleCompleteTodotaskProp}>make it as not completed</button> :
+            <button onClick={handleCompleteTodotaskProp}>complete todo</button>}
         <button onClick={handleDeleteButton}>delete</button>
 
     </div>
